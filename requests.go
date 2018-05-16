bacec0e2a02bc748300d5d1425dfffaf72ac264a
Ido Perlmuter <ido@scalock.com>
2018-05-16 16:12:44 +0300

    Add retries, size limit, timeouts, etc.
    This commit includes the following changes to the requests library:
    
     - The ability to retry requests is added. The client itself can be
       configured to retry every request, and/or specific requests can have
       their own retry limit as well. Apart from the number of retries, the
       strategy cannot be modified currently. The delay between retries will
       be 2 seconds at first, then doubled each attempt (so 2s, 4s, 8s, 16s,
       etc.).
    
     - The ability to reject responses the exceed a certain size. Currently
       this can only be done an a per-request basis.
    
     - The ability to set a timeout on a per-request basis (rather than on
       the client itself) is added, and the implementation itself is
       modified to use Go's context package. If not provided, a 2 minute
       timeout is used for all requests. This is in contrast to Go's default
       HTTP client, which does not have a default timeout at all.
    
     - The ability to provide a custom response body handler is added. It is
       used whenever the response content type is something that requests
       cannot parse by itself.
    
     - The library now exports the default transport used by it via the
       DefaultTransport() function. This allows usage of the same transport
       throught a code-base that still uses net/http directly in various
       locations.
    
     - The client now implements the Do() method so that it can be used by
       other libraries that require an HTTP client. Since Do() receives a
       *http.Request object, only some of the features provided by the
       library are usable through Do().

--- requests.go
+++ requests.go
@@ -2,9 +2,11 @@ package requests
 
 import (
 	"bytes"
+	"context"
 	"crypto/tls"
 	"encoding/base64"
 	"encoding/json"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net"
@@ -16,6 +18,7 @@ import (
 	"unicode"
 
 	"github.com/pkg/errors"
+	"go.uber.org/zap"
 )
 
 type authType string
@@ -39,7 +42,10 @@ type HTTPClient struct {
 	customHeaders map[string]string     // default headers for all requests
 	accept        string                // default expected response content type for all requests
 	timeout       time.Duration         // default timeout for all requests
+	retryLimit    uint8                 // default retry limit for all requests
 	errorHandler  func(io.Reader) error // default error handler for all requests returning unexpected status
+	bodyHandler   BodyHandlerFunc       // default body handler for requests (when Content-Type is not automatically handled by this library)
+	logger        *zap.Logger           // logger to use (only debug messages are printed by the library; defaults to noop logger)
 }
 
 // HTTPRequest represents a single HTTP request to the web service defined
@@ -64,13 +70,31 @@ type HTTPRequest struct {
 	authHeader     string                // header used for authentication (defaults to 'Authorization')
 	customHeaders  map[string]string     // headers for the request (headers from the client will be used as well, but headers defined here take precedence)
 	accept         string                // expected response content type
+	timeout        time.Duration         // timeout for the request
+	retryLimit     uint8                 // retry limit for the request
+	sizeLimit      int64                 // reject responses whose body size exceeds this value
 	errorHandler   func(io.Reader) error // error handler for request if returned status is not the expected one
+	bodyHandler    BodyHandlerFunc       // body handler for request (when Content-Type is not automatically handled by this library)
+	logger         *zap.Logger           // logger to use (only debug messages are printed by the library; defaults to noop logger)
 }
+
+// BodyHandlerFunc is a function that processes the request body and reads
+// it into the target variable. It receives the content type of the response
+// (from the Content-Type header), the body reader, and the target variable
+// (which is whatever was provided to the Into() method). It is not necessary
+// to close the body or read it to its entirety.
+type BodyHandlerFunc func(contentType string, body io.Reader, target interface{}) error
+
+var (
+	ErrSizeExceeded   = errors.New("response size exceeded limit")
+	ErrTimeoutReached = errors.New("timeout reached")
+)
 
 func NewClient(baseURL string) *HTTPClient {
 	return &HTTPClient{
 		baseURL:  strings.TrimSuffix(baseURL, "/"),
 		authType: noAuth,
+		timeout:  time.Minute * 2,
 	}
 }
 
@@ -80,9 +104,25 @@ func (cli *HTTPClient) Accept(accept string) *HTTPClient {
 }
 
 func (cli *HTTPClient) Timeout(dur time.Duration) *HTTPClient {
-	// nil out existing HTTP client
 	cli.timeout = dur
 	return cli
+}
+
+func (cli *HTTPClient) RetryLimit(limit uint8) *HTTPClient {
+	cli.retryLimit = limit
+	return cli
+}
+
+func (cli *HTTPClient) Logger(logger *zap.Logger) *HTTPClient {
+	cli.logger = logger
+	return cli
+}
+
+func (cli *HTTPClient) getLogger() *zap.Logger {
+	if cli.logger == nil {
+		cli.logger = zap.NewNop()
+	}
+	return cli.logger
 }
 
 func (cli *HTTPClient) BasicAuth(user, pass, header string) *HTTPClient {
@@ -106,47 +146,129 @@ func (cli *HTTPClient) ErrorHandler(handler func(io.Reader) error) *HTTPClient {
 	return cli
 }
 
+func (cli *HTTPClient) BodyHandler(handler BodyHandlerFunc) *HTTPClient {
+	cli.bodyHandler = handler
+	return cli
+}
+
 func (cli *HTTPClient) NoTLSVerify(enabled bool) *HTTPClient {
-	// nil out existing HTTP client
-	cli.httpCli = nil
+	if cli.httpCli != nil {
+		transport, ok := cli.httpCli.Transport.(*http.Transport)
+		if ok {
+			transport.TLSClientConfig.InsecureSkipVerify = enabled
+		} else {
+			// just nil out the client so a new one is created
+			cli.httpCli = nil
+		}
+	}
 	cli.noTLSVerify = enabled
 	return cli
 }
 
 func (cli *HTTPClient) NewRequest(method, path string) *HTTPRequest {
-	if cli.httpCli == nil {
-		tlsConfig := &tls.Config{InsecureSkipVerify: cli.noTLSVerify}
-
-		// this is a modification of Golang's default HTTP transport
-		// (https://golang.org/pkg/net/http/#RoundTripper) to ignore
-		// invalid certificates (note that this is using go 1.6's
-		// default transport, there are more configurations in 1.7)
-		var transport http.RoundTripper = &http.Transport{
-			Proxy: http.ProxyFromEnvironment,
-			DialContext: (&net.Dialer{
-				Timeout:   30 * time.Second,
-				KeepAlive: 30 * time.Second,
-				DualStack: true,
-			}).DialContext,
-			MaxIdleConns:          10,
-			IdleConnTimeout:       60 * time.Second,
-			TLSHandshakeTimeout:   10 * time.Second,
-			ExpectContinueTimeout: 1 * time.Second,
-			TLSClientConfig:       tlsConfig,
-		}
-
-		cli.httpCli = &http.Client{
-			Timeout:   cli.timeout,
-			Transport: transport,
-		}
-	}
-
 	return &HTTPRequest{
 		cli:    cli,
 		path:   path,
 		method: method,
+		logger: cli.getLogger().With(
+			zap.String("path", path),
+			zap.String("method", method),
+		),
 	}
 }
+
+func (cli *HTTPClient) Do(req *http.Request) (*http.Response, error) {
+	return cli.retryRequest(
+		context.Background(),
+		cli.logger,
+		req,
+		cli.timeout,
+		cli.retryLimit+1,
+	)
+}
+
+func (cli *HTTPClient) retryRequest(
+	parentCtx context.Context,
+	logger *zap.Logger,
+	req *http.Request,
+	timeout time.Duration,
+	attempts uint8,
+) (res *http.Response, err error) {
+	if cli.httpCli == nil {
+		cli.httpCli = &http.Client{
+			Transport: DefaultTransport(cli.noTLSVerify),
+		}
+	}
+
+	if logger == nil {
+		logger = zap.NewNop()
+	}
+
+	delay := 2 * time.Second
+
+	for attempt := uint8(1); attempt <= attempts; attempt++ {
+		res, err = cli.doRequest(parentCtx, req, timeout)
+		// we retry requests if an error is returned from net/http, or if
+		// the status of the response is 502, 503 or 504, which are all proxy
+		// errors that may be temporary
+		if err == nil &&
+			res.StatusCode != http.StatusBadGateway &&
+			res.StatusCode != http.StatusServiceUnavailable &&
+			res.StatusCode != http.StatusGatewayTimeout {
+			break
+		}
+
+		// failed this attempt, sleep 2*attempt seconds and try again
+		if attempt < attempts {
+			logger.Debug(
+				"Request failed, will retry",
+				zap.Uint8("attempt", attempt),
+				zap.Uint8("limit", attempts),
+				zap.Duration("delay", delay),
+				zap.Error(err),
+			)
+			time.Sleep(delay)
+			delay *= 2
+		}
+	}
+
+	return res, err
+}
+
+func (cli *HTTPClient) doRequest(
+	parentCtx context.Context,
+	req *http.Request,
+	timeout time.Duration,
+) (res *http.Response, err error) {
+	if cli.httpCli == nil {
+		cli.httpCli = &http.Client{
+			Transport: DefaultTransport(cli.noTLSVerify),
+		}
+	}
+
+	// initiate the request
+	req = req.WithContext(parentCtx)
+
+	if timeout > 0 {
+		ctx, _ := context.WithTimeout(parentCtx, timeout)
+		req = req.WithContext(ctx)
+	}
+
+	res, err = cli.httpCli.Do(req)
+	if err != nil {
+		if urlErr, ok := err.(*url.Error); ok {
+			if urlErr.Err == context.DeadlineExceeded {
+				err = ErrTimeoutReached
+			}
+		}
+	}
+
+	return res, err
+}
+
+/******************************************************************************/
+/*                                REQUESTS                                    */
+/******************************************************************************/
 
 func (req *HTTPRequest) QueryParam(key, value string) *HTTPRequest {
 	if req.queryParams == nil {
@@ -179,6 +301,21 @@ func (req *HTTPRequest) JSONBody(body interface{}) *HTTPRequest {
 
 func (req *HTTPRequest) Accept(accept string) *HTTPRequest {
 	req.accept = accept
+	return req
+}
+
+func (req *HTTPRequest) Timeout(dur time.Duration) *HTTPRequest {
+	req.timeout = dur
+	return req
+}
+
+func (req *HTTPRequest) RetryLimit(limit uint8) *HTTPRequest {
+	req.retryLimit = limit
+	return req
+}
+
+func (req *HTTPRequest) SizeLimit(limit int64) *HTTPRequest {
+	req.sizeLimit = limit
 	return req
 }
 
@@ -231,7 +368,16 @@ func (req *HTTPRequest) ErrorHandler(handler func(io.Reader) error) *HTTPRequest
 	return req
 }
 
+func (req *HTTPRequest) BodyHandler(handler BodyHandlerFunc) *HTTPRequest {
+	req.bodyHandler = handler
+	return req
+}
+
 func (req *HTTPRequest) Run() error {
+	return req.RunContext(context.Background())
+}
+
+func (req *HTTPRequest) RunContext(ctx context.Context) error {
 	// did we fail during building the request? if so, return the error
 	if req.err != nil {
 		return req.err
@@ -244,6 +390,9 @@ func (req *HTTPRequest) Run() error {
 
 	// build the request URL from the client's base URL and the request's
 	// path, plus add any query parameters defined
+	if !strings.HasPrefix(req.path, "/") {
+		req.path = fmt.Sprintf("/%s", req.path)
+	}
 	reqURL := req.cli.baseURL + req.path
 	if req.queryParams != nil {
 		if queryParams := req.queryParams.Encode(); queryParams != "" {
@@ -313,9 +462,26 @@ func (req *HTTPRequest) Run() error {
 	}
 	r.Header.Add("Accept", req.accept)
 
-	// initiate the request
-	res, err := req.cli.httpCli.Do(r)
+	// how many attempts are we gonna make?
+	var attempts = uint8(1)
+	if req.retryLimit > 0 {
+		attempts += req.retryLimit
+	} else if req.cli.retryLimit > 0 {
+		attempts += req.cli.retryLimit
+	}
+
+	// what is the request timeout?
+	timeout := req.timeout
+	if req.timeout == 0 && req.cli.timeout > 0 {
+		timeout = req.cli.timeout
+	}
+
+	// run the request
+	res, err := req.cli.retryRequest(ctx, req.logger, r, timeout, attempts)
 	if err != nil {
+		if err == ErrTimeoutReached {
+			return err
+		}
 		return errors.Wrap(err, "request failed")
 	}
 
@@ -326,6 +492,10 @@ func (req *HTTPRequest) Run() error {
 		io.Copy(ioutil.Discard, res.Body)
 		res.Body.Close()
 	}()
+
+	if req.sizeLimit > 0 && res.ContentLength > req.sizeLimit {
+		return ErrSizeExceeded
+	}
 
 	// are there any headers from the response we need to read?
 	if req.headersInto != nil {
@@ -360,30 +530,77 @@ func (req *HTTPRequest) Run() error {
 	// what are we loading the response into and how? make sure we're only
 	// doing this if there is a response content
 	if res.StatusCode != http.StatusNoContent && req.into != nil {
-		switch req.accept {
-		case "text/plain":
-			v := reflect.ValueOf(req.into)
-			if v.Kind() != reflect.Ptr || v.IsNil() {
-				return errors.Wrap(err, "invalid target variable")
-			}
+		var handler BodyHandlerFunc
+		if req.bodyHandler != nil {
+			handler = req.bodyHandler
+		} else if req.cli.bodyHandler != nil {
+			handler = req.cli.bodyHandler
+		} else {
+			handler = defaultBodyHandler
+		}
+		err = handler(res.Header.Get("Content-Type"), res.Body, req.into)
+	}
+	return err
+}
 
-			data, err := ioutil.ReadAll(res.Body)
-			if err != nil {
-				return errors.Wrap(err, "failed reading server response")
-			}
+func defaultBodyHandler(
+	contentType string,
+	body io.Reader,
+	target interface{},
+) (err error) {
+	cType := strings.Split(contentType, ";")
+	switch cType[0] {
+	case "text/plain":
+		v := reflect.ValueOf(target)
+		if v.Kind() != reflect.Ptr || v.IsNil() {
+			return errors.Wrap(err, "invalid target variable")
+		}
 
-			switch v := req.into.(type) {
-			case *string:
-				*v = strings.TrimRightFunc(string(data), unicode.IsSpace)
-			default:
-				return errors.New("target variable is not a string pointer")
-			}
+		data, err := ioutil.ReadAll(body)
+		if err != nil {
+			return errors.Wrap(err, "failed reading server response")
+		}
+
+		switch v := target.(type) {
+		case *string:
+			*v = strings.TrimRightFunc(string(data), unicode.IsSpace)
 		default:
-			err = json.NewDecoder(res.Body).Decode(req.into)
-			if err != nil {
-				return errors.Wrap(err, "failed decoding server response")
-			}
+			return errors.New("target variable is not a string pointer")
+		}
+	default:
+		// default to JSON, regardless of returned Content-Type
+		err = json.NewDecoder(body).Decode(target)
+		if err != nil {
+			return errors.Wrap(err, "failed decoding server response")
 		}
 	}
+
 	return nil
+}
+
+func DefaultTransport(tlsNoVerify bool) http.RoundTripper {
+	// this is a modification of Golang's default HTTP transport
+	// (https://golang.org/pkg/net/http/#RoundTripper) with options
+	// to ignore invalid or self-signed TLS certificates and to configure
+	// timeouts.
+	// see https://blog.cloudflare.com/the-complete-guide-to-golang-net-http-timeouts/
+	// for more details about client timeouts
+	var transport http.RoundTripper = &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second, // time spent establishing a TCP connection (if a new one is needed)
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second, // time spent performing the TLS handshake
+		ResponseHeaderTimeout: 10 * time.Second, // time spent reading the headers of the response
+		ExpectContinueTimeout: 1 * time.Second,
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: tlsNoVerify,
+		},
+	}
+
+	return transport
 }
--- requests_test.go
+++ requests_test.go
@@ -2,9 +2,14 @@ package requests
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
+
+	"github.com/pkg/errors"
 )
 
 func TestPlainTextResponse(t *testing.T) {
@@ -106,18 +111,118 @@ func TestErrorResponse(t *testing.T) {
 	}
 }
 
-func TestNoTLSVerify(t *testing.T) {
-	cli := NewClient("")
-	cli.NewRequest("GET", "/")
+//func TestNoTLSVerify(t *testing.T) {
+//cli := NewClient("")
+//cli.NewRequest("GET", "/")
 
-	transport := cli.httpCli.Transport.(*http.Transport)
-	if transport.TLSClientConfig.InsecureSkipVerify {
-		t.Fatal("Transport should verify TLS connections but isn't")
+//transport := cli.httpCli.Transport.(*http.Transport)
+//if transport.TLSClientConfig.InsecureSkipVerify {
+//t.Fatal("Transport should verify TLS connections but isn't")
+//}
+
+//cli.NoTLSVerify(true).NewRequest("GET", "/")
+//transport = cli.httpCli.Transport.(*http.Transport)
+//if !transport.TLSClientConfig.InsecureSkipVerify {
+//t.Fatal("Transport shouldn't verify TLS connections but is")
+//}
+//}
+
+func TestResponseSizeLimit(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		fmt.Fprintln(w, "Lorem ipsum dolor sit amet")
+	}))
+	defer ts.Close()
+
+	var data string
+
+	cli := NewClient(ts.URL)
+	err := cli.NewRequest("GET", "/").SizeLimit(5).Into(&data).Run()
+	if err != ErrSizeExceeded {
+		t.Fatal("Response size was not limited as expected")
+	}
+}
+
+func TestRequestTimeout(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(2 * time.Second)
+		w.Header().Set("Content-Type", "text/plain")
+		fmt.Fprintln(w, "Lorem ipsum dolor sit amet")
+	}))
+	defer ts.Close()
+
+	err := NewClient(ts.URL).
+		NewRequest("GET", "/").
+		Timeout(1 * time.Second).
+		Run()
+	if err == nil {
+		t.Fatal("Response succeeded but should have timed out")
+	} else if err != ErrTimeoutReached {
+		t.Fatalf("Request failed but not with timeout error: %s", err)
+	}
+}
+
+func TestRequestRetries(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		fmt.Fprintln(w, "Lorem ipsum dolor sit amet")
+	}))
+	ts.Close() // this is on purpose
+
+	cli := NewClient(ts.URL)
+	start := time.Now()
+	err := cli.NewRequest("GET", "/").RetryLimit(2).Run()
+	end := time.Now()
+	if err == nil {
+		t.Fatal("Request did not fail as expected")
+	} else if end.Sub(start) < 6*time.Second {
+		t.Fatal("Request was not retried as expected")
+	}
+}
+
+func TestCustomBodyHandler(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/x.rot13")
+		fmt.Fprintln(w, "Hello, World")
+	}))
+	defer ts.Close()
+
+	var data []byte
+
+	err := NewClient(ts.URL).
+		NewRequest("GET", "/").
+		BodyHandler(func(cType string, reader io.Reader, target interface{}) error {
+			if cType != "application/x.rot13" {
+				return errors.Errorf("unexpected content type %s returned", cType)
+			}
+			b, err := ioutil.ReadAll(reader)
+			if err != nil {
+				return errors.Wrap(err, "failed reading response body")
+			}
+			t := target.(*[]byte)
+			*t = rot13(b[:len(b)-1]) // chomp newline
+			return nil
+		}).
+		Into(&data).
+		Run()
+	if err != nil {
+		t.Fatalf("Failed custom body handler: %s", err)
+	} else if string(data) != "Uryyb, Jbeyq" {
+		t.Fatalf("Body handler returned wrong content: %q", string(data))
+	}
+}
+
+func rot13(b []byte) []byte {
+	for i, r := range b {
+		switch {
+		case r >= 'A' && r <= 'Z':
+			b[i] = 'A' + (((r - 'A') + 13) % 26)
+		case r >= 'a' && r <= 'z':
+			b[i] = 'a' + (((r - 'a') + 13) % 26)
+		default:
+			b[i] = r
+		}
 	}
 
-	cli.NoTLSVerify(true).NewRequest("GET", "/")
-	transport = cli.httpCli.Transport.(*http.Transport)
-	if !transport.TLSClientConfig.InsecureSkipVerify {
-		t.Fatal("Transport shouldn't verify TLS connections but is")
-	}
+	return b
 }
